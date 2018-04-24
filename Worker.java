4c5480b27a6b99aa65f74e6e7d58ebe827520a49
Stanislav Radchenko <Stanislav.Radchenko@ibm.com>
2018-04-24 12:16:51 +0300

    worker OCR start working like shit

--- Worker.java
+++ Worker.java
@@ -1,41 +1,26 @@
 package com.company;
 
+
+
 import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
-import com.lowagie.text.pdf.codec.Base64.InputStream;
+import com.asprise.ocr.Ocr;
 
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
+
 
 public class Worker {
     public static void main(String [] args){
-    	URL url;
-    	Tesseract tesseract = new Tesseract();
-    	java.io.InputStream in;
-		try {
-			url = new URL("http://files.microscan.com/Technology/OCR/ocr_font_examples.jpg");
-			in = url.openStream();
-			Files.copy(in, Paths.get("fuckYou.jpg"), StandardCopyOption.REPLACE_EXISTING);
-	    	in.close();
-	    	String text = tesseract.doOCR(new File ("fuckYou.jpg"));
-	    	System.out.println(text);
-	    	
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TesseractException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	Ocr.setUp();
+		Ocr ocr = new Ocr();
+		ocr.startEngine("eng", Ocr.SPEED_SLOW);
+		//URL url = new URL("http://www.idautomation.com/ocr-a-and-ocr-b-fonts/new_sizes_ocr.png");
+		//BufferedImage image = ImageIO.read(url);
+		String test = ocr.recognize(new File [] {new File("3d-text.jpg")}, Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT);
+		System.out.println(test);
+		ocr.stopEngine();
+    	
+    	
+    	
     	
     	
         
